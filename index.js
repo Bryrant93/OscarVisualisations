0a683fb1c1910be007f1f23e97f333b86186afec
Robert Bryant <bryantr4@cardiff.ac.uk>
2022-03-28 11:39:58 +0100

    penultimateultimate build

--- index.js
+++ index.js
@@ -41,18 +41,18 @@ d3.csv("https://raw.githubusercontent.com/Bryrant93/DataVisCW2/main/winnersFrame
 
   // Add Y axis
   const y1 = d3.scaleLinear()
-    .domain([0, 6])
+    .domain([0, 10])
     .range([ height1, 0 ]);
     // add the Y gridlines
   svg1.append("g")			
     .attr("class", "grid1")
     .call(d3.axisLeft(y1)
-        .ticks(6)
+        .ticks(10)
         .tickSize(-width1)
         .tickFormat("")
     )
   svg1.append("g")
-    .call(d3.axisLeft(y1).ticks(6))
+    .call(d3.axisLeft(y1).ticks(10))
     .style("color","white");
   
   // Handmade legend
@@ -72,9 +72,9 @@ d3.csv("https://raw.githubusercontent.com/Bryrant93/DataVisCW2/main/winnersFrame
     .attr("fill", "white")
     .text("No. of years the highest rated film won Best Picture");
   
-    svg1.append("line").attr("x1", 883).attr("y1",324.5).attr("x2",1060).attr("y2",324.5).style("stroke", "white").style("stroke-dasharray", "8,4").style("stroke-width", 3).style("opacity",0).transition().duration(1500).style("opacity",1)
-    svg1.append("text").attr("x", 883).attr("y", 305).text("Only two Academy Awards Ceremonies ").style("font-size", "12.5px").attr("alignment-baseline","middle").attr("fill", "#f5f5f5").style("opacity",0).transition().duration(1500).style("opacity",1)
-    svg1.append("text").attr("x", 883).attr("y", 317).text("have taken place so far this decade.").style("font-size", "12.5px").attr("alignment-baseline","middle").attr("fill", "#f5f5f5").style("opacity",0).transition().duration(1500).style("opacity",1)
+    svg1.append("line").attr("x1", 883).attr("y1",392).attr("x2",1060).attr("y2",392).style("stroke", "white").style("stroke-dasharray", "8,4").style("stroke-width", 3).style("opacity",0).transition().duration(1500).style("opacity",1)
+    svg1.append("text").attr("x", 883).attr("y", 367).text("Only two Academy Awards Ceremonies ").style("font-size", "12.5px").attr("alignment-baseline","middle").attr("fill", "#f5f5f5").style("opacity",0).transition().duration(1500).style("opacity",1)
+    svg1.append("text").attr("x", 883).attr("y", 382).text("have taken place so far this decade.").style("font-size", "12.5px").attr("alignment-baseline","middle").attr("fill", "#f5f5f5").style("opacity",0).transition().duration(1500).style("opacity",1)
 
   // Another scale for subgroup position?
   const xSubgroup = d3.scaleBand()
@@ -98,15 +98,15 @@ d3.csv("https://raw.githubusercontent.com/Bryrant93/DataVisCW2/main/winnersFrame
       .selectAll("rect")
       .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
       .join("rect")
-        .attr("x", d => xSubgroup(d.key))
+        .attr("x", d => xSubgroup(d.key)+4)
         .attr("y", 490)
-        .attr("width", xSubgroup.bandwidth())
+        .attr("width", xSubgroup.bandwidth()*.9)
         .attr("height", 0)
         .attr("fill", d => color1(d.key))
       .transition().duration(1250)
-        .attr("x", d => xSubgroup(d.key))
+        .attr("x", d => xSubgroup(d.key)+4)
         .attr("y", d => y1(d.value))
-        .attr("width", xSubgroup.bandwidth())
+        .attr("width", xSubgroup.bandwidth()*.9)
         .attr("height", d => height1 - y1(d.value))
         .attr("fill", d => color1(d.key));
 
